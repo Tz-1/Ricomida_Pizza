59c19103c83de524abc1aa2640d34130e130cdb0
Tz-1 <zetz97@gmail.com>
2024-08-20 21:09:55 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+});
+
+$(document).ready(function() {
+    $("#enviarCorreo").click(function(){
+        alert("El correo fue enviado correctamente...");
+    })
+})
+
+$(document).ready(function() {
+    $("h5").on("dblclick", function(){
+        $(this).css("color", "red");
+    });
+});
+
+$(document).ready(function() {
+    $(".crdtlt").click(function(){
+        $("p").toggle();
+    })
+})
